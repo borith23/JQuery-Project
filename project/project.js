4c27988fccd756be5147a7363ffd0ed1ee0b34e3
borith.moek <borith.moek@PNC.LAN>
2019-12-10 09:19:50 +0700

    Updata code

--- project/project.js
+++ project/project.js
@@ -1,15 +1,71 @@
-$(document).ready(function(){
+function getUrl(){
     var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
-    $.ajax({
-        dataType: "json",
-        url: url,
-        success: function(data){
-            data.recipes.forEach(element => {
-                console.log(element.ingredients);
-            });
-        },
-        error: function(){
-            console.error("Can not get data!");
-        }  
+    return url;
+}
+$(document).ready(function(){
+    requestApi();
+    $('#recipe').on('change', () => {
+        var recipeId = $('#recipe').val();
+        getRecipe(recipeId);
+       
     });
 });
+function requestApi(){
+    $.ajax({
+        dataType: 'json',
+        url: getUrl(),
+        success: (data) => chooseRecipe(data.recipes),
+        error: () => console.log("Can not get data"),
+    });
+}
+
+// 
+var allData = [];
+function chooseRecipe(recipe){
+    allData = recipe;
+    var option = "";
+    recipe.forEach(item => {
+       option += `
+            <option value="${item.id}">${item.name}</option>
+           
+       `;
+
+    });
+    $('#recipe').append(option);
+}
+
+
+//
+function getRecipe(id){
+    allData.forEach(item => {
+        if(item.id == id){
+            eachRecipe(item.name, item.iconUrl);
+            eachIngredient(item.ingredients);
+        }
+    })
+}
+//
+function eachRecipe(name, img){
+    var result = "";
+    result += `
+        <img src="${img}" width="170">
+        <h3>${name}</h3>
+    `;
+    $('#recipe-result').html(result);
+}
+
+//
+function eachIngredient( ing){
+    var result = "";
+    ing.forEach(item => {
+    result += `
+        <tr>
+            <td><img src="${item.iconUrl}" width="100"></td>
+            <td>${item.name}</td>
+            <td>${item.quantity}</td>
+            <td>${item.unit[0]}</td>
+        </tr>
+    `;
+    });
+    $('#ingredient-result').html(result);
+}
